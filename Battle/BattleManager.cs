cc8e7548fdb289f0bd20bb7b9c2c5eb601f58390
psw221 <tpdnddlek@gmail.com>
2016-05-18 00:45:50 +0900

    go to bed
    go to bed

--- Battle/BattleManager.cs
+++ Battle/BattleManager.cs
@@ -23,6 +23,7 @@ public class BattleManager {
     // 내가 바꿈 SMK
     // cjh1205 : 나도 바꿈
     // cjh1205 : 왜 안되지
+    // 자러갑시다 님들 - psw221
 
 	// Update is called once per frame
 	public void CheckBattle () {    //todo : 이부분을 호출하는 부분이 필요함
